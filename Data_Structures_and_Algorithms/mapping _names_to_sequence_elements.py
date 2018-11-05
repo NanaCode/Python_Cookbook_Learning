3061a72da9eafd656a0f86afc1ac3a87601d571b
NanaCode <2503605326@qq.com>
2018-11-05 17:34:14 +0800

    disscussion part of mapping names to sequence elements

--- Data_Structures_and_Algorithms/mapping _names_to_sequence_elements.py
+++ Data_Structures_and_Algorithms/mapping _names_to_sequence_elements.py
@@ -45,5 +45,48 @@ def compute_cost(records):
         total += s.shares * s.price
     return total
 
-# lack of discussion
 
+# namedtuple is immutable
+s = Stock('KFC', 100, 123.5)
+print(s)
+print(s.shares)
+# s.shares = 75  # 报错：AttributeError: can't set attribute
+# Stock(name='KFC', shares=100, price=123.5)
+# 100
+# Traceback (most recent call last):
+# File
+# "C:/Users/Administrator/Desktop/Python_Cookbook_Learning/Data_Structures_and_Algorithms/mapping _names_to_sequence_elements.py", line
+# 52, in < module >
+# s.shares = 75
+# AttributeError: can't set attribute
+
+# change of the attributes can be done by using the _replace() method of a namedtuple instance
+# which makes an entirely new namedtuple with specified values
+s = s._replace(shares=75)
+print(s)
+# Stock(name='KFC', shares=75, price=123.5)
+
+print('-'*20)
+# subtle use of the _replace() method: populate named tuples that have optional or missing fields
+from collections import namedtuple
+
+Stock = namedtuple('Stock', ['name', 'shares', 'price', 'date', 'time'])
+
+# create a prototype instance
+stock_prototype = Stock('', 0, 0.0, None, None)
+
+
+# Function to convert a dictionary to a Stock
+def dict_to_stock(s):
+    return stock_prototype._replace(**s)
+
+
+a = {'name': 'MC', 'shares': 99, 'price': 123.8}
+print(dict_to_stock(a))
+b = {'name': 'MC', 'shares': 99, 'price': 123.8, 'date': '12/17/2012'}
+print(dict_to_stock(b))
+# Stock(name='MC', shares=99, price=123.8, date=None, time=None)
+# Stock(name='MC', shares=99, price=123.8, date='12/17/2012', time=None)
+
+# if goal is to define an efficient data structure where you will be changing various instance
+# attributes, consider defining a class using __slots__ instead of using namedtuple.
