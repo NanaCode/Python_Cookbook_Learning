18506e7772d790adce20bfa0e3c3f05775c4a59b
NanaCode <2503605326@qq.com>
2018-11-17 23:27:59 +0800

    join() method to combine strings

---
+++ Strings_and_text/Combining_and_concatenating_strings.py
@@ -0,0 +1,8 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'Nana'
+__date__ = '2018/11/17 23:24'
+
+# use join() method to combine the strings in a sequence or iterable
+parts = ['Is', 'Chicago', 'Not', 'Chicago?']
+print(''.join(parts))
+# IsChicagoNotChicago?
