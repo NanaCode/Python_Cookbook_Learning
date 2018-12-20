9dfb2b472ea76ba3484a09333b229801c1369252
NanaCode <2503605326@qq.com>
2018-12-20 23:53:18 +0800

    handling html and xml entities in text

--- Strings_and_text/handling_html_and xml_entities_in_text.py
+++ Strings_and_text/handling_html_and xml_entities_in_text.py
@@ -1,3 +1,14 @@
 # _*_ coding: utf-8 _*_
 __author__ = 'Nana'
-__date__ = '2018/12/19 22:17' 
+__date__ = '2018/12/19 22:17'
+
+# Use the html.escape() to replace special characters such as < or >
+s = 'Elements are written as "<tag>text</tag>".'
+import html
+print(s)
+# Elements are written as "<tag>text</tag>".
+print(html.escape(s))
+# Elements are written as &quot;&lt;tag&gt;text&lt;/tag&gt;&quot;.
+print(html.escape(s, quote=False))
+# Elements are written as "&lt;tag&gt;text&lt;/tag&gt;".
+
